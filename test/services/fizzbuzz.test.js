e41cbde0f5fb31f8e9de05e59efeb25f8b138623
Hirepan E <ginebrasyrel@gmail.com>
2022-05-28 10:56:12 -0500

    Limpiando librerías de fizzbuzz test

--- test/services/fizzbuzz.test.js
+++ test/services/fizzbuzz.test.js
@@ -1,9 +1,6 @@
 const fizzbuzzService = require("../../lib/services/fizzbuzzService");
-const reader = require("../../lib/utils/reader")
-const explorers = reader.readJsonFile("/home/syrel/launchX_playbook/weekly_mission_4/Refactoring/explorers.json")
-const filter3 = explorers.filter((explorer)=> explorer.score === 3)
-        const filter3Modified = fizzbuzzService.applyValidationInExplorer(filter3[0])
-        console.log(filter3Modified);
+const explorers = require("../../explorers.json")
+
 describe('Testing para FizzbuzzService',()=>{
     test('1. Crear propiedad objeto.trick = "Fizz" si, parameter%3 === 0', ()=>{
         const filter3 = explorers.filter((explorer)=> explorer.score %3 === 0)
